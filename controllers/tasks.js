310a37659d9fcb64d961379e5c7a7f5845d6ee5c
Bekarion <beqarioni@yahoo.co.uk>
2022-11-07 16:01:46 +0400

    finished the project

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,25 +1,49 @@
 const Task = require("../models/Task")
+const asyncWrapper = require("../middleware/async")
+const { createCustomError } = require("../errors/custom-error")
 
-const getAllTasks = (req, res, next) => {
-  res.send("all tasks")
-}
+const getAllTasks = asyncWrapper(async (req, res, next) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
 
-const createTask = async (req, res, next) => {
+const createTask = asyncWrapper(async (req, res, next) => {
   const task = await Task.create(req.body)
   res.status(201).json(task)
-}
+})
 
-const getTask = (req, res, next) => {
-  res.json({ id: req.params.id })
-}
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id: ${taskID}`, 404))
+  }
+  res.status(200).json({ task })
+})
 
-const updateTask = (req, res, next) => {
-  res.send("update task")
-}
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
 
-const deleteTask = (req, res, next) => {
-  res.send("delete task")
-}
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  if (!task) {
+    return next(createCustomError(`No task with id: ${taskID}`, 404))
+  }
+
+  res.status(200).json({ task })
+})
+
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOneAndDelete({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id: ${taskID}`, 404))
+  }
+  res.status(200).json({ task })
+})
 
 module.exports = {
   getAllTasks,
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    minlength: [3, "name can't be less than 3 characters"],
+    maxlength: [40, "name can not be more than 40 characters"],
+  },
+  completed: {
+    type: Boolean, 
+    default: false,
+  },
 })
 
 module.exports = mongoose.model("Task", TaskSchema)
